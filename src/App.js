939e24372c6b0d710efb081de93b828e5770c83a
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-04-18 17:10:19 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Name from "./Name";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Name/>
     </div>
   );
 }
---
+++ src/Name.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+function Name(){
+    return<div>My name is Elkana Kibet Sang</div>
+}
+export default Name
